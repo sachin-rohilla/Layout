8f5e45e194fe6a4cb0b76c050d4f3dbf6bdf2f48
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-13 13:50:12 +0530

    wheather

--- src/Components/Axios.js
+++ src/Components/Axios.js
@@ -1,55 +1,47 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { useState } from 'react';
-
-
-
+import { useState } from "react";
 
 const Axios = () => {
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
 
-    
+  const getWetherDetails = (cityName) => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          cityName +
+          "&appid=e4b2c3bd2f677e854d83635398fe3a76"
+      )
+      .then((res) => {
+        console.log("response", res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
 
-const [inputCity, setInputCity] = useState("");
-const [data, setData] = useState({});
+  const handleInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
 
-const getWetherDetails = (cityName) => {
-  
- 
-  axios
-    .get(
-      "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=e4b2c3bd2f677e854d83635398fe3a76"
-    )
-    .then((res) => {
-      console.log("response", res.data);
-      setData(res.data);
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
-    
-};
+  const handleSearch = () => {
+    getWetherDetails(inputCity);
+  };
 
-const handleInput = (e) => {
-  console.log("value", e.target.value);
-  setInputCity(e.target.value);
-};
-
-const handleSearch = () => {
-  getWetherDetails(inputCity);
-};
-
-
- 
-
-  
-  
-  
   return (
     <>
-      <TextField id="standard-basic"  variant="standard"  value={inputCity} onChange={handleInput}/>
+      <TextField
+        id="standard-basic"
+        variant="standard"
+        value={inputCity}
+        onChange={handleInput}
+      />
       <Button color="primary" onClick={handleSearch}>
         Search
       </Button>
@@ -57,11 +49,10 @@ const handleSearch = () => {
         {data.name}
       </Typography>
       <Typography variant="h4" component="div" gutterBottom>
-        {((data?.main?.temp) - 273.15).toFixed(2)}°C
+        {(data?.main?.temp - 273.15).toFixed(2)}°C
       </Typography>
-     
     </>
   );
-}
+};
 
-export default Axios
+export default Axios;
